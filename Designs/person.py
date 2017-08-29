07a399de86c3606be50a08ab292ae7e0caac341a
andersonmasese <andersonmasese@gmail.com>
2017-08-28 22:12:04 -0700

    [Chore]

--- Designs/person.py
+++ Designs/person.py
@@ -1,12 +1,19 @@
 class Person:
     def register(self,username,emailaddress,password):
-        with open('user.txt', 'w') as f:
+        with open('user.txt', 'a') as f:
             f.write(username)
+            f.write('\n')
             f.write(emailaddress)
+            f.write('\n')
             f.write(password)
+            f.write('\n')
         
 
     def validator(self):
+        file_contents=[]
         with open('user.txt','r') as f:
-            return f
-        
+            for lines in f:
+                #file_contents.append(lines)
+                #return file_contents
+                return lines
+            
