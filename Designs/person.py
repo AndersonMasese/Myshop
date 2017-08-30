53b25ff8d828313fe6c88ad50ce933ab78c0db73
andersonmasese <andersonmasese@gmail.com>
2017-08-29 20:59:59 -0700

    [Fix] Complete integration between flask main app and the templates

--- Designs/person.py
+++ Designs/person.py
@@ -1,5 +1,9 @@
 class Person:
+    '''
+    Class is controller of all login and registration activities
+    '''
     def register(self,username,emailaddress,password):
+        '''method inserts the individual's details into a file'''
         with open('user.txt', 'a') as f:
             f.write(username)
             f.write('\n')
@@ -10,6 +14,7 @@ class Person:
         
 
     def validator(self):
+        '''method opens file and returns the individual user contents'''
         file_contents=[]
         with open('user.txt','r') as f:
             for lines in f:
--- Designs/shopping_list.py
+++ Designs/shopping_list.py
@@ -1,12 +1,27 @@
 from person import Person
 
-class ShoppingList:
+class ShoppingList(Person):
+    '''class models the creation of a shopping list and logging of users to a local file
+    for purposes of sharing shopping lists'''
+
     
     def create(self,list_name,description):
-        '''This function creates a file which is the shopping list'''
+        '''This method creates a file which is the shopping list'''
         with open(list_name+'.txt','a') as sl:
             sl.write(description)
             sl.write('\n')
+        #as a list is created, a file keeping track of names of shoppping lists
+        with open('shoplogger.txt','a') as f:
+            f.write(list_name)
+            f.write('\n')
+
+    def share_container(self,file_name):
+        '''This method maintains  a file which logs all shared shopping lists'''
+
+        with open('shared_lists.txt','a') as f:
+            f.write(file_name)
+            f.write('\n')
+
     def show(self):
         '''show function reads and returns the records in a file'''
         slist=[]
